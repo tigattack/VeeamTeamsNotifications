d5bf6179deb3fe49fb9d6028c7a76d6aa1135eb8
tigattack <10629864+tigattack@users.noreply.github.com>
2018-01-29 17:20:32 +0000

    Data sizes, job duration and reporting layout.
    Moved $session.BackupStats.BackupSize and $session.BackupStats.DataSize to $JobSize and $TransfSize for ease of use in calculations.
    Series' of if statements then use these to determine the best unit in which to report the job and transferred data sizes. If there's no match (unlikely) then it fallback to reporting in bytes.
    
    Added job duration to the report.
    
    Changed emojis from an angry face for warning, green heart for success and fire for failure. This appears rather unprofessional and the meaning is not immediately obvious. Warning is now a yellow warning triangle, success is a green check-mark, and failure is a red cross.
    
    Changed report section titles (Job, Status, Details) from italic to bold. It didn't make much sense for a title to be italic and having it bold makes the report much more clear.

--- SlackVeeamAlertSender.ps1
+++ SlackVeeamAlertSender.ps1
@@ -33,25 +33,94 @@ while ($session.IsCompleted -eq $false) {
 $Status = $session.Result
 $JobName = $session.Name.ToString().Trim()
 $JobType = $session.JobTypeString.Trim()
+[Float]$JobSize = $session.BackupStats.BackupSize
+[Float]$TransfSize = $session.BackupStats.DataSize
+
+# Report job/data size in B, KB, MB, GB, or TB depending on completed size.
+## Job size
+If([Float]$JobSize -lt 1KB) {
+    [String]$JobSizeRound = [Float]$JobSize
+    [String]$JobSizeRound += ' B'
+}
+ElseIf([Float]$JobSize -lt 1MB) {
+    [Float]$JobSize = [Float]$JobSize / 1KB
+    [String]$JobSizeRound = [math]::Round($JobSize,2)
+    [String]$JobSizeRound += ' KB'
+}
+ElseIf([Float]$JobSize -lt 1GB) {
+    [Float]$JobSize = [Float]$JobSize / 1MB
+    [String]$JobSizeRound = [math]::Round($JobSize,2)
+    [String]$JobSizeRound += ' MB'
+}
+ElseIf([Float]$JobSize -lt 1TB) {
+    [Float]$JobSize = [Float]$JobSize / 1GB
+    [String]$JobSizeRound = [math]::Round($JobSize,2)
+    [String]$JobSizeRound += ' GB'
+}
+ElseIf([Float]$JobSize -ge 1TB) {
+    [Float]$JobSize = [Float]$JobSize / 1TB
+    [String]$JobSizeRound = [math]::Round($JobSize,2)
+    [String]$JobSizeRound += ' TB'
+}
+### If no match then report in bytes
+Else{
+    [String]$JobSizeRound = [Float]$JobSize
+    [String]$JobSizeRound += ' B'
+}
+## Transfer size
+If([Float]$TransfSize -lt 1KB) {
+    [String]$TransfSizeRound = [Float]$TransfSize
+    [String]$TransfSizeRound += ' B'
+}
+ElseIf([Float]$TransfSize -lt 1MB) {
+    [Float]$TransfSize = [Float]$TransfSize / 1KB
+    [String]$TransfSizeRound = [math]::Round($TransfSize,2)
+    [String]$TransfSizeRound += ' KB'
+}
+ElseIf([Float]$TransfSize -lt 1GB) {
+    [Float]$TransfSize = [Float]$TransfSize / 1MB
+    [String]$TransfSizeRound = [math]::Round($TransfSize,2)
+    [String]$TransfSizeRound += ' MB'
+}
+ElseIf([Float]$TransfSize -lt 1TB) {
+    [Float]$TransfSize = [Float]$TransfSize / 1GB
+    [String]$TransfSizeRound = [math]::Round($TransfSize,2)
+    [String]$TransfSizeRound += ' GB'
+}
+ElseIf([Float]$TransfSize -ge 1TB) {
+    [Float]$TransfSize = [Float]$TransfSize / 1TB
+    [String]$TransfSizeRound = [math]::Round($TransfSize,2)
+    [String]$TransfSizeRound += ' TB'
+}
+### If no match then report in bytes
+Else{
+    [String]$TransfSizeRound = [Float]$TransfSize
+    [String]$TransfSizeRound += ' B'
+}
+
+# Job duration
+$Duration = $session.Info.EndTime - $session.Info.CreationTime
+$TimeSpan = $Duration
+$Duration = '{0:00}h {1:00}m {2:00}s' -f $TimeSpan.Hours, $TimeSpan.Minutes, $TimeSpan.Seconds
 
 # Switch on the session status
 switch ($Status) {
     None {$emoji = ':thought _ balloon: '}
-    Warning {$emoji = ':angry: '}
-    Success {$emoji = ':green_heart: '}
-    Failed {$emoji = ':fire: '}
+    Warning {$emoji = ':warning: '}
+    Success {$emoji = ':white_check_mark:  '}
+    Failed {$emoji = ':x: '}
     Default {$emoji = ''}
 }
 
 # Build the details string
-$details  = "Backup Size - " + [String]$session.BackupStats.BackupSize + " / Data Size - " + [String]$session.BackupStats.DataSize + " / Dedup Ratio - " + [String]$session.BackupStats.DedupRatio + " / Compress Ratio - " + [String]$session.BackupStats.CompressRatio
+$details  = "Backup Size - " + [String]$JobSizeRound + " / Transferred Data - " + [String]$TransfSizeRound + " / Dedup Ratio - " + [String]$session.BackupStats.DedupRatio + " / Compress Ratio - " + [String]$session.BackupStats.CompressRatio + " / Duration - " + $Duration
 
 # Build the payload
 $slackJSON = @{}
 $slackJSON.channel = $config.channel
 $slackJSON.username = $config.service_name
 $slackJSON.icon_url = $config.icon_url
-$slackJSON.text = $emoji + '*Job:* ' + $JobName + "`n" + $emoji + '*Status:* ' + $Status + "`n" + $emoji + '*Details:* '  + $details
+$slackJSON.text = $emoji + '**Job:** ' + $JobName + "`n" + $emoji + '**Status:** ' + $Status + "`n" + $emoji + '**Details:** '  + $details
 
 # Build the web request
 $webReq=@{
